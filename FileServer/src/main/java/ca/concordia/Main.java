4547b9dacc6a4bac3ede4086048c3d27d11b5345
Arnav Singh <159664776+arnav-singh-ahlawat@users.noreply.github.com>
2025-10-31 19:38:25 -0400

    Update Main.java

--- FileServer/src/main/java/ca/concordia/Main.java
+++ FileServer/src/main/java/ca/concordia/Main.java
@@ -4,7 +4,7 @@ import ca.concordia.server.FileServer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         FileServer server = new FileServer(12345, "filesystem.dat", 10 * 128);
         // Start the file server
